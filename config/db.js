321139b3c02c8b72d14ec5fba48719905e2c1f25
ranjan <ranjanpandey11111@gmail.com>
2019-07-29 12:27:54 -0700

    makes changes for ui changes

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
-// Set the connection string based from the config vars of the production server
-// To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
-//mongodb://<dbuser>:<dbpassword>@ds117729.mlab.com:17729/test3769
+
+
 
 module.exports = {
-  db: "mongodb://ranjan:ranjan123@ds255857.mlab.com:55857/telephonedirectory"
+  db: `mongodb://${username}:${password}@${hostname}:${port}/${databasename}`
 };
